8cf6c45cdd803376b07d412c708e4a62e9509ede
Muhammad Habib <habibsitompul99@gmail.com>
2023-07-23 20:53:39 +0700

    add channel range test

--- goroutines/channel/channel_test.go
+++ goroutines/channel/channel_test.go
@@ -1,6 +1,7 @@
 package channel
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -12,6 +13,7 @@ func ChannelIn(channel chan<- string)  {
 
 func ChannelOut(channel <-chan string)  {
 	time.Sleep(2 * time.Second)
+	fmt.Println(<-channel)
 }
 
 func Test_ChannelInOut(t *testing.T) {
@@ -21,4 +23,30 @@ func Test_ChannelInOut(t *testing.T) {
 
 	time.Sleep(3 * time.Second)
 	close(channel)
+}
+
+func proccessChannel(channel <-chan string, result chan<- string, group int)  {
+	for data := range channel {
+		result <- data + fmt.Sprintf(" - done - group=%d",group)
+	}
+}
+
+func Test_ChannelRange(t *testing.T)  {
+	channelIn := make(chan string, 5)
+	result := make(chan string, 5)
+
+	for i := 0; i < 5; i++ {
+		go proccessChannel(channelIn, result, i+1)
+	}
+	
+	for i := 0; i < 15; i++ {
+		data := fmt.Sprintf("data ke %d", i+1)
+		channelIn <- data
+	}
+	
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-result)
+	}
+	close(channelIn)
+	close(result)
 }
