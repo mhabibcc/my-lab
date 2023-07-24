3542b0a1f38ee3b16378327b4034d821c038850f
habibaladin <132416731+habibaladin@users.noreply.github.com>
2023-07-24 18:29:47 +0700

    add lock mutex

--- goroutines/channel/race_channel_test.go
+++ goroutines/channel/race_channel_test.go
@@ -2,16 +2,34 @@ package channel
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 )
 
 func Test_RaceCondition(t *testing.T) {
 	var x = 0
-	for i := 0; i <= 1000; i++ {
+	for i := 0; i < 1000; i++ {
 		go func() {
-			for j := 0; j <= 100; j++ {
+			for j := 0; j < 100; j++ {
 				x = x + 1
+			}
+		}()
+	}
+	time.Sleep(time.Second * 5)
+	fmt.Println("Counter : ", x)
+}
+
+// Mutual Exclusion
+func Test_Mutex(t *testing.T) {
+	var x = 0
+	var mutex sync.Mutex
+	for i := 0; i < 1000; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				mutex.Lock()
+				x = x + 1
+				mutex.Unlock()
 			}
 		}()
 	}
